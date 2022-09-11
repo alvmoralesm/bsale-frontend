8af8cd33d3a6f8362896dadfa6ba708a3065d91a
Alvaro Morales <amorales@integralit.cl>
2022-09-11 10:56:19 -0300

    path fixed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });
